ce5c5f9cf5213b2e190f76829bbf67edfaf09609
boo <booblegum42@gmail.com>
2025-09-17 14:34:27 +0700

    add tasks to Part03

--- Part03/weather_async.py
+++ Part03/weather_async.py
@@ -24,3 +24,17 @@ async def read_weather(city: str):
     """Маршрут для получения погоды по названию города."""
     weather_data = await get_weather(city)
     return weather_data
+
+
+# TODO-1: сервис api.weatherapi.com отправляет слишком много данных.
+#  измените код weather_async.py, так чтобы наше API возвращало JSON только с ключами:
+#  "name" "region" "country" "temp_c" "feelslike_c"
+#  используйте модель Pydantic для конвертации данных
+
+# TODO-2: доработайте код так, чтобы при запросе на несуществующий город, ваше API возвращало 404
+
+
+# TODO-3: сервис https://www.cbr-xml-daily.ru/daily_json.js (не требует api-key)
+#  возвращает курсы валют на текущую дату
+#  создайте handler для proxy-api, который будет возвращать только курс ЕВРО с параметрами:
+#  "CharCode" "Name" "Value"
